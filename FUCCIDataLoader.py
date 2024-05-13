727521b134c0942c841483bb77eb60c3fa3b97b8
Julius Zimmermann <julius.zimmermann@unipv.it>
2024-05-13 21:00:12 +0200

    enable different file endings for source and target

--- FUCCIDataLoader.py
+++ FUCCIDataLoader.py
@@ -8,11 +8,21 @@ from aicsimageio import AICSImage
 class FUCCIDataset(Dataset):
     """A PyTorch dataset to load cell images and nuclei masks"""
 
-    def __init__(self, root_dir,
-                 source_channels: tuple, target_channels: tuple, transform=None, img_transform=None):
-        # TODO enable different file edings for source and target
-        self.root_dir = os.path.join("/group/dl4miacourse/projects/FUCCI", root_dir) # the directory with all the training samples
-        self.video_files = os.listdir(os.path.join(self.root_dir, "Source"))  # list the videos
+    def __init__(
+        self,
+        root_dir,
+        source_channels: tuple,
+        target_channels: tuple,
+        transform=None,
+        img_transform=None,
+        target_ending=".tif"
+    ):
+        self.root_dir = os.path.join(
+            "/group/dl4miacourse/projects/FUCCI", root_dir
+        )  # the directory with all the training samples
+        self.video_files = os.listdir(
+            os.path.join(self.root_dir, "Source")
+        )  # list the videos
         self.transform = (
             transform  # transformations to apply to both inputs and targets
         )
@@ -37,19 +47,20 @@ class FUCCIDataset(Dataset):
 
         for video_file_base in self.video_files:
             video_file = os.path.join(self.root_dir, "Source", video_file_base)
-            target_file = os.path.join(self.root_dir, "Target", video_file_base)
+            target_file_base = os.path.splitext(video_file_base)[0] + target_ending
+            target_file = os.path.join(self.root_dir, "Target", target_file_base)
             video = AICSImage(video_file)
             target = AICSImage(target_file)
             n_frames_source = video.dims.T
             n_frames_target = target.dims.T
             if not n_frames_target == n_frames_source:
-                raise ValueError(f"Video {video_file_base} does not have "
-                                  "the same frames in target and source")
+                raise ValueError(
+                    f"Video {video_file_base} does not have "
+                    "the same frames in target and source"
+                )
             self.open_videos.append(video)
             self.open_targets.append(target)
             self.frames_per_video.append(n_frames_source)
-
-
 
     # get the total number of samples
     def __len__(self):
@@ -74,13 +85,17 @@ class FUCCIDataset(Dataset):
         if isinstance(self.source_channels, tuple):
             if len(self.source_channels) > 1:
                 return_dims = "CYX"
-        source_frames = self.open_videos[video_idx].get_image_data(return_dims, C=self.source_channels, T=frame_idx)
+        source_frames = self.open_videos[video_idx].get_image_data(
+            return_dims, C=self.source_channels, T=frame_idx
+        )
 
         if isinstance(self.target_channels, tuple):
             if len(self.target_channels) > 1:
                 return_dims = "CYX"
 
-        target_frames = self.open_targets[video_idx].get_image_data(return_dims, C=self.target_channels, T=frame_idx)
+        target_frames = self.open_targets[video_idx].get_image_data(
+            return_dims, C=self.target_channels, T=frame_idx
+        )
         if self.transform is not None:
             seed = torch.seed()
             torch.manual_seed(seed)
@@ -91,8 +106,7 @@ class FUCCIDataset(Dataset):
             source_frames = self.img_transform(source_frames)
         return source_frames, target_frames
 
-        
-        
+
 '''
 def load_nd2_file(nd2_file):
     """Load the file (TODO flat field correction?)"""
